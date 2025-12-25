b714f0f8a1299b36b732f12af4debacbe163f4da
Ammar Afridi <ammar.afridi95@gmail.com>
2025-12-25 15:17:53 +0400

    Add AppRoutes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,10 @@
 import 'react-tooltip/dist/react-tooltip.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { HelmetProvider } from 'react-helmet-async';
 import { AuthProvider } from './context/AuthContext';
-
-import AppLayout from './components/AppLayout';
-import Dashboard from './pages/Dashboard';
-import Login from './features/auth/pages/Login';
-import NotFound from './pages/NotFound';
-
-// Dummy Ticket Routes
-import DummyTickets from './features/dummy-tickets/pages/DummyTickets';
-import DummyTicketDetail from './features/dummy-tickets/pages/DummyTicketDetail';
-import SendEmail from './features/dummy-tickets/pages/SendEmail';
-
-// Protection Routes
-import ProtectedRoute from './pages/ProtectedRoute';
-import AdminRoute from './pages/AdminRoute';
-
-// Users
-import Users from './features/users/pages/Users';
-import CreateUser from './features/users/pages/CreateUser';
-import UpdateUser from './features/users/pages/UpdateUser';
-import MyAccount from './features/account/pages/MyAccount';
-
-// Blog
-import Blogs from './features/blog/pages/Blogs';
-import CreateBlogPost from './features/blog/pages/CreateBlogPost';
-import UpdateBlogPost from './features/blog/pages/UpdateBlogPost';
+import AppRoutes from './app/AppRoutes';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -40,7 +15,6 @@ const queryClient = new QueryClient({
   },
 });
 
-// App.jsx
 export default function App() {
   return (
     <>
@@ -49,34 +23,7 @@ export default function App() {
         <ReactQueryDevtools initialIsOpen={false} />
         <HelmetProvider>
           <AuthProvider>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/login" element={<Login />} />
-
-                <Route element={<ProtectedRoute />}>
-                  <Route element={<AppLayout />}>
-                    <Route index element={<Navigate to="dashboard" replace />} />
-                    <Route path="dashboard" element={<Dashboard />} />
-
-                    <Route element={<AdminRoute />}>
-                      <Route path="dummy-tickets" element={<DummyTickets />} />
-                      <Route path="dummy-tickets/:sessionId" element={<DummyTicketDetail />} />
-                      <Route path="send-email" element={<SendEmail />} />
-                      <Route path="users" element={<Users />} />
-                      <Route path="users/create" element={<CreateUser />} />
-                      <Route path="users/:username" element={<UpdateUser />} />
-                    </Route>
-
-                    <Route path="account" element={<MyAccount />} />
-                    <Route path="blogs" element={<Blogs />} />
-                    <Route path="blogs/create" element={<CreateBlogPost />} />
-                    <Route path="blogs/:id" element={<UpdateBlogPost />} />
-                  </Route>
-                </Route>
-
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </BrowserRouter>
+            <AppRoutes />
           </AuthProvider>
         </HelmetProvider>
       </QueryClientProvider>
