81c9b4e4f112773a5541f6e3fd1f75a4eac14c6f
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-05 17:06:44 +0400

    Use formData for image updating

--- src/features/blog/pages/UpdateBlogPost.jsx
+++ src/features/blog/pages/UpdateBlogPost.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { useBlog } from '../hooks/useBlog';
 import { useUpdateBlog } from '../hooks/useUpdateBlog';
+import { usePublishBlog } from '../hooks/usePublishBlog';
 import BlogForm from '../components/BlogForm';
 import Breadcrumb from '../../../components/Breadcrumb';
 import PageHeading from '../../../components/PageHeading';
@@ -14,6 +15,7 @@ export default function UpdateBlogPost() {
   const { id } = useParams();
   const { blog, isErrorBlog, isLoadingBlog } = useBlog(id);
   const { updateBlog, isUpdatingBlog } = useUpdateBlog();
+  const { publishBlog, isPublishingBlog } = usePublishBlog();
 
   const { register, handleSubmit, control, reset } = useForm({
     defaultValues: {
@@ -28,20 +30,42 @@ export default function UpdateBlogPost() {
   });
 
   function onSubmit(data) {
-    const updatedData = { ...data, content: editorRef.current.getContent() };
-    updateBlog({ id, blogData: updatedData });
+    const editorContent = editorRef.current ? editorRef.current.getContent() : '';
+    const formData = new FormData();
+
+    formData.append('metaTitle', data.metaTitle || '');
+    formData.append('metaDescription', data.metaDescription || '');
+    formData.append('title', data.title || '');
+    formData.append('slug', data.slug || '');
+    formData.append('excerpt', data.excerpt || '');
+    formData.append('content', editorContent);
+
+    if (data.tags && data.tags.length > 0) {
+      data.tags.forEach((tag) => formData.append('tags', tag));
+    }
+
+    if (data.coverImage && data.coverImage[0]) {
+      formData.append('newCoverImage', data.coverImage[0]);
+    }
+
+    console.log(formData);
+
+    if (blog?.status === 'draft' || blog?.status === 'archive') {
+      publishBlog({ id });
+    } else {
+      updateBlog({ id, blogData: formData });
+    }
   }
 
   useEffect(() => {
     if (blog) {
       reset({
-        metaTitle: blog.metaTitle,
-        metaDescription: blog.metaDescription,
-        title: blog.title,
-        slug: blog.slug,
-        excerpt: blog.excerpt,
-        tags: blog.tags,
-        content: blog.content,
+        metaTitle: blog.metaTitle || '',
+        metaDescription: blog.metaDescription || '',
+        title: blog.title || '',
+        slug: blog.slug || '',
+        excerpt: blog.excerpt || '',
+        tags: blog.tags || [],
       });
     }
   }, [blog, reset]);
