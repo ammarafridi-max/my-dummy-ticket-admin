877bacd3870d7e16709f31c3a7ba1129612330bc
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-30 10:42:06 +0400

    Reduce size

--- src/components/PageHeading.jsx
+++ src/components/PageHeading.jsx
@@ -1,3 +1,3 @@
 export default function PageHeading({ children, className }) {
-  return <h1 className={`font-medium text-[32px] mb-3 ${className}`}>{children}</h1>;
+  return <h1 className={`font-medium text-[28px] mb-3 ${className}`}>{children}</h1>;
 }
